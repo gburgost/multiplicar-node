07ae20ce398a1714cdbfe951b1f82f8369ee0aa6
pelaogon <pelaogon@gmail.com>
2019-05-28 11:49:43 -0400

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,42 @@
+// requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+    console.log('=============='.green);
+    console.log(`tabla de ${base}`.green);
+    console.log('=============='.green);
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base * i}`);
+    }
+}
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            // let resultado = i * 2;
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${ base }-al-${ limite }.txt`)
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
